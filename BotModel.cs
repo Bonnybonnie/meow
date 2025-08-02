3b7d68508f864dd62de35132f2dd76f1390cb439
Bonnybonnie <cookiecupcake0524@gmail.com>
2025-08-01 20:41:13 -0400

    Add files via upload

---
+++ BotModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+using upeko.Services;
+
+namespace upeko.Models
+{
+    public partial class BotModel
+    {
+        public Guid Guid { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; } = "New Bot";
+
+        public string? IconUri { get; set; }
+
+        public string? Version { get; set; }
+
+        public string? PathUri { get; set; }
+    }
+
+    [JsonSerializable(typeof(BotModel))]
+    [JsonSerializable(typeof(Guid))]
+    [JsonSerializable(typeof(ConfigModel))]
+    [JsonSerializable(typeof(ReleaseModel))]
+    [JsonSerializable(typeof(ReleaseAsset))]
+    [JsonSourceGenerationOptions(
+        PropertyNameCaseInsensitive = true,
+        WriteIndented = true
+    )]
+    public partial class SourceJsonSerializer : JsonSerializerContext;
+}
